4cfa5bc3e4ba532b0d13176e6e6eb46372a3191b
Rus Hughes <russell.hughes@gmail.com>
2019-08-11 17:56:15 +0200

    s12 l370

--- app/controllers/logins_controller.rb
+++ app/controllers/logins_controller.rb
@@ -5,11 +5,21 @@ class LoginsController < ApplicationController
   end
 
   def create
-
+    student = Student.find_by(email: params[:logins][:email].downcase)
+    if student && student.authenticate(params[:logins][:password])
+      flash[:notice] = "Successfully logged in"
+      session[:student_id] = student.id
+      redirect_to student
+    else
+      flash.now[:notice] = "Invalid user"
+      render 'new'
+    end
   end
-  
-  def destroy
 
+  def destroy
+    session[:student_id] = nil
+    flash[:notice] = "You have logged out"
+    redirect_to root_path
   end
 
 end
