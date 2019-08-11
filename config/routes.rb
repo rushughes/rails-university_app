44b23408f5aeefd5360f03adb1938d40e3cda861
Rus Hughes <russell.hughes@gmail.com>
2019-08-11 17:37:15 +0200

    s12 l368

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
-Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+# frozen_string_literal: true
 
+Rails.application.routes.draw do
   root 'courses#index'
   get 'courses/new', to: 'courses#new'
   get 'about', to: 'pages#about'
 
   resources :students, except: [:destroy]
 
+  get 'login', to: 'logins#new'
+  post 'login', to: 'logins#create'
+  delete 'logout', to: 'logins#destroy'
 end
