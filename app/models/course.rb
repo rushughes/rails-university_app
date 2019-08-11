0ecd19de185a166080a3df67104e0715012d4f85
Rus Hughes <russell.hughes@gmail.com>
2019-08-11 18:25:24 +0200

    s12 l373

--- app/models/course.rb
+++ app/models/course.rb
@@ -2,4 +2,6 @@ class Course < ApplicationRecord
   validates :short_name, presence: true, length: {minimum: 5, maximum: 5 }
   validates :name, presence: true, length: {minimum: 1}
   validates :description, presence: true, length: {minimum: 1}
+  has_many :student_courses
+  has_many :students, through: :student_courses
 end
