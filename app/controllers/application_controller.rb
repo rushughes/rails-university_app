faa585228b1c479d800107be48c70d07a109a524
Rus Hughes <russell.hughes@gmail.com>
2019-08-11 18:11:04 +0200

    s12 l371

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  before_action :require_user
+
   helper_method :current_user, :logged_in?
 
   def current_user
@@ -8,6 +10,14 @@ class ApplicationController < ActionController::Base
   end
 
   def logged_in?
-    !!current_user
+    # !!current_user
+    !current_user.nil?
+  end
+
+  def require_user
+    if !logged_in?
+      flash[:notice] = 'You must be logged in to perform that action'
+      redirect_to login_path
+    end
   end
 end
--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,6 +1,8 @@
 class StudentsController < ApplicationController
 
+  skip_before_action :require_user, only: [:new, :create]
   before_action :set_student, only: [:show, :edit, :update]
+  before_action :require_same_student, only: [:edit, :update]
 
   def index
     @students = Student.all
@@ -24,6 +26,7 @@ class StudentsController < ApplicationController
   end
 
   def edit
+
   end
 
   def update
@@ -45,4 +48,10 @@ class StudentsController < ApplicationController
     @student = Student.find(params[:id])
   end
 
+  def require_same_student
+    if current_user != @student
+      flash[:notice] = "You can only edit your own profile"
+      redirect_to student_path(current_user)
+    end
+  end
 end
